c718c9ecf77cc48b8f12e32687bdfaa3b061a188
mfellows <mfellows@nrcan.gc.ca>
2017-11-28 11:49:03 -0800

    Added regular arc license to manager - use instead of importing arcpy in other modules in order to gain retry support. Attempts to release the regular arc license after exiting the context block, but in practice it's not really possible to release licenses other than extensions like GeoStats.

--- preprocess_tools/licensemanager.py
+++ preprocess_tools/licensemanager.py
@@ -1,4 +1,3 @@
-import arcpy
 import logging
 import time
 import os
@@ -8,18 +7,100 @@ from contextlib import contextmanager
 os.environ["FOR_DISABLE_CONSOLE_CTRL_HANDLER"] = "1"
 os.environ["FOR_IGNORE_EXCEPTIONS"] = "1"
 
-GEOSTATS = "GeoStats"
+class Extensions(object):
+    def __init__(self): raise RuntimeError("Not instantiable")
+
+    GEOSTATS = "GeoStats"
+    SPATIAL = "Spatial"
+    
+    @staticmethod
+    def manages(name):
+        return name in [Extensions.GEOSTATS, Extensions.SPATIAL]
+        
+    @staticmethod
+    def checkout(extension):
+        try:
+            import archook
+            archook.get_arcpy()
+            import arcpy
+            arcpy.CheckOutExtension(extension)
+            return arcpy
+        except:
+            return None
+            
+    @staticmethod
+    def release(extension):
+        try:
+            arcpy.CheckInExtension(extension)
+        except:
+            pass
+
+  
+class Products(object):
+    def __init__(self): raise RuntimeError("Not instantiable")
+
+    ARC         = "Arc"
+    ARCINFO     = "ArcInfo"
+    ARCEDITOR   = "ArcEditor"
+    ARCVIEW     = "ArcView"
+    
+    @staticmethod
+    def manages(name):
+        return name in [Products.ARC, Products.ARCINFO, Products.ARCEDITOR, Products.ARCVIEW]
+        
+    @staticmethod
+    def checkout(name):
+        try:
+            if name == Products.ARCINFO:   import arcinfo
+            if name == Products.ARCEDITOR: import arceditor
+            if name == Products.ARCVIEW:   import arcview
+            import archook
+            archook.get_arcpy()
+            import arcpy
+            return arcpy if arcpy.ProductInfo() != "NotInitialized" else None
+        except:
+            return None
+    
+    @staticmethod
+    def release(name):
+        try:
+            if name == Products.ARCINFO:   del arcinfo
+            if name == Products.ARCEDITOR: del arceditor
+            if name == Products.ARCVIEW:   del arcview
+            del arcpy
+        except:
+            pass
+            
 
 @contextmanager
-def arc_license(extension):
+def arc_license(product_or_extension):
+    mgr = None
+    for license_manager in (Products, Extensions):
+        if license_manager.manages(product_or_extension):
+            mgr = license_manager
+    
+    if not mgr:
+        logging.fatal("Tried to acquire unsupported license: {}".format(product_or_extension))
+        return
+    
+    wait_time = 60
+    max_retries = 60**2 * 12 / wait_time # Retry for up to 12 hours
+    
+    attempt = 0
     try:
-        while True:
+        license = None
+        while not license and attempt < max_retries:
             try:
-                arcpy.CheckOutExtension(extension)
-                break
+                license = mgr.checkout(product_or_extension)
             except:
-                logging.info("Waiting for {} license...".format(extension))
-                time.sleep(10)
-        yield
+                logging.info("Waiting for {} license...".format(product_or_extension))
+                time.sleep(60)
+            finally:
+                attempt += 1
+        
+        if not license and attempt >= max_retries:
+            logging.fatal("Failed to acquire {} after {} attempts.".format(product_or_extension, attempt))
+            
+        yield license
     finally:
-        arcpy.CheckInExtension(extension)
+        mgr.release(product_or_extension)
