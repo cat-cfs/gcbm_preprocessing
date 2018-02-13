179bb8ff679a1e7d81860234a66e63a8b3aaa884
Scott Morken <smorken@nrcan.gc.ca>
2018-02-12 17:09:20 -0800

    path registry for managing many dependent paths

---
+++ pathregistry.py
@@ -0,0 +1,77 @@
+import os, logging
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.edges = []
+    def addEdge(self, node):
+        self.edges.append(node)
+
+class PathRegistry(object):
+    def __init__(self, region_name, config):
+
+        if "Region_Name" in config:
+            raise ValueError("Region_name is a reserved path item")
+        config["Region_Name"] = [region_name]
+
+        nodes = {}
+        for k,v in config.items():
+            nodes[k] = Node(k)
+
+        for k,v in config.items():
+            for d in self.get_dependent_paths(v):
+                if not k in nodes or not d in nodes:
+                    raise ValueError("specified path reference not found '{}'"
+                                     .format(k))
+
+                nodes[k].addEdge(nodes[d])
+
+        PathTokens = {}
+        for k,v in config.items():
+            if not k in PathTokens:
+                resolved = []
+                unresolved = []
+                self.dep_resolve(nodes[k], resolved, unresolved)
+                for a in resolved:
+                    if a.name in PathTokens:
+                        continue
+                    tokens = self.sub_dependent_paths(config[a.name], PathTokens)
+                    PathTokens[a.name] = tokens
+
+        self.Paths={}
+        for k,v in PathTokens.items():
+            self.Paths[k] = os.path.join(*v)
+            logging.info("{k}: '{v}'".format(k=k, v= self.Paths[k]))
+
+    def is_dependent_token(self, token):
+        return token.startswith("${") and token.endswith("}")
+
+    def strip_dependent_token(self, token):
+        return token.replace("${", "").replace("}", "")
+
+    def sub_dependent_paths(self, p_dependent, path_collection):
+        tokens = []
+        for t in p_dependent:
+            if self.is_dependent_token(t):
+                tokens.extend(path_collection[self.strip_dependent_token(t)])
+            else:
+                tokens.append(t)
+        return tokens
+
+    def get_dependent_paths(self, pathList):
+        depNames = [x for x in pathList if self.is_dependent_token(x)]
+        return [self.strip_dependent_token(x) for x in depNames]
+
+    def dep_resolve(self, node, resolved, unresolved):
+        '''
+        dependency resolver algorithm copied from this article
+        https://www.electricmonk.nl/docs/dependency_resolving_algorithm/dependency_resolving_algorithm.html
+        '''
+        unresolved.append(node)
+        for edge in node.edges:
+                if edge not in resolved:
+                        if edge in unresolved:
+                            raise Exception('Circular reference detected: %s -> %s' % (node.name, edge.name))
+                        self.dep_resolve(edge, resolved, unresolved)
+        resolved.append(node)
+        unresolved.remove(node)
