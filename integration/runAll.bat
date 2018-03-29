a440ed2e57a9c18909d3faa6ef53ae5ed433d139
Scott Morken <smorken@nrcan.gc.ca>
2018-03-28 18:00:46 -0700

    integration batch file

--- integration/runAll.bat
+++ integration/runAll.bat
@@ -4,4 +4,5 @@ python ..\regiongridder.py --pathRegistry .\pathRegistry.json --preprocessorConf
 python ..\rollback.py --pathRegistry .\pathRegistry.json --preprocessorConfig .\preprocessorConfig.json --subRegionConfig .\subRegionConfig.json --subRegionNames "Boundary"
 python ..\historicprocessor.py --pathRegistry .\pathRegistry.json --preprocessorConfig .\preprocessorConfig.json --subRegionConfig .\subRegionConfig.json --runtiler --subRegionNames "Boundary"
 python ..\futureprocessor.py --pathRegistry .\pathRegistry.json --futureConfig .\futureConfig.json --subRegionConfig .\subRegionConfig.json --subRegionNames "Boundary"
-python ..\recliner2GCBM.py --configTemplatePath .\recliner2GCBMConfigTemplate.json --pathRegistry .\pathRegistry.json --futureConfig .\futureConfig.json --subRegionConfig .\subRegionConfig.json --subRegionNames "Boundary"
+python ..\recliner2GCBM.py --pathRegistry .\pathRegistry.json --futureConfig .\futureConfig.json --subRegionConfig .\subRegionConfig.json --subRegionNames "Boundary" 
+python ..\runupdategcbmconfig.py --pathRegistry .\pathRegistry.json --futureConfig .\futureConfig.json --subRegionConfig .\subRegionConfig.json --subRegionNames "Boundary" 
