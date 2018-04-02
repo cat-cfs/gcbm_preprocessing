804d5a861b7b861d3e009637c879b5c858832aee
Simon Norris <snorris@hillcrestgeo.ca>
2018-04-01 17:31:53 -0700

    remove util file

--- grid/grid_inventory.py
+++ grid/grid_inventory.py
@@ -10,6 +10,24 @@ import pgdata
 import util
 
 
+def parallel_tiled(db_url, sql, block, n_subs=2):
+    """
+    Create a connection and execute query for specified block.
+    n_subs is the number of places in the sql query that should be
+    substituted by the block id
+    """
+    # create a new connection
+    db = pgdata.connect(db_url, multiprocessing=True)
+    # As we are explicitly splitting up our job we don't want the database to try
+    # and manage parallel execution of these queries within these connections.
+    # Turn off this connection's parallel execution for pg version >= 10:
+    version_string = db.query('SELECT version()').fetchone()[0]
+    major_version_number = int(version_string.split(' ')[1].split('.')[0])
+    if major_version_number >= 10:
+        db.execute("SET max_parallel_workers_per_gather = 0")
+    db.execute(sql, (block,) * n_subs)
+
+
 class GridInventory(object):
 
     def __init__(
@@ -82,7 +100,7 @@ class GridInventory(object):
         # note that self.resolution is not currently passed to grid creation
         sql = self.db.queries['load_grid']
         blocks = [b for b in self.db['preprocessing.blocks'].distinct('block_id')]
-        func = partial(util.parallel_tiled, self.db.url, sql, n_subs=2)
+        func = partial(parallel_tiled, self.db.url, sql, n_subs=2)
         pool = multiprocessing.Pool(self.n_processes)
         pool.map(func, blocks)
         pool.close()
@@ -106,7 +124,7 @@ class GridInventory(object):
             return ValueError('Centroid inventory gridding is not currently supported')
         sql = self.db.queries[query]
         blocks = [b for b in self.db['preprocessing.blocks'].distinct('block_id')]
-        func = partial(util.parallel_tiled, self.db.url, sql, n_subs=1)
+        func = partial(parallel_tiled, self.db.url, sql, n_subs=1)
         pool = multiprocessing.Pool(self.n_processes)
         pool.map(func, blocks)
         pool.close()
