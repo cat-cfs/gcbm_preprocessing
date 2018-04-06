3d5ebc332b2d80922847b598ea0cb22bf3995ebd
Simon Norris <snorris@hillcrestgeo.ca>
2018-04-06 08:57:42 -0700

    apply calc rollback age to pg table

--- rollback/update_inventory.py
+++ rollback/update_inventory.py
@@ -2,172 +2,9 @@ import csv
 import random
 import numpy as np
 import os
-import sys
-import inspect
 import logging
-from dbfread import DBF
 
-class CalculateDistDEdifference(object):
-    def __init__(self, arcpy, inventory_workspace, inventory_year, inventory_field_names):
-        self.arcpy = arcpy
-        self.inventory_workspace = inventory_workspace
-        self.inventory_year = inventory_year
-        self.inventory_field_names = inventory_field_names
-
-        # VARIABLES:
-        self.disturbedInventory = "DisturbedInventory"
-        self.disturbedInventory_layer = "disturbedInventory_layer"
-
-    def calculateDistDEdifference(self):
-        logging.info("computing disturbance/date of establishment differences")
-        self.arcpy.env.workspace = self.inventory_workspace
-        self.arcpy.env.overwriteOutput = True
-        #local variables
-
-        self.inv_dist_dateDiff = self.inventory_field_names['dist_date_diff']
-
-        self.makeLayers()
-        self.calculateFields()
-
-    def makeLayers(self):
-        logging.info("creating working layer")
-        self.arcpy.MakeFeatureLayer_management(self.disturbedInventory, self.disturbedInventory_layer)
-
-    def calculateFields(self):
-        logging.info("computing fields")
-
-        #Calculate date of establishment and difference between date of establishment (inventory) and date of stand-replacing disturbance
-        logging.info('Calculating date of establishment and difference between date of establishment (inventory) and date of disturbance')
-        cur = self.arcpy.UpdateCursor(self.disturbedInventory_layer)
-        for row in cur:
-            # The variable 'age' will get the value from the column
-            age = row.getValue(self.inventory_field_names['age'])
-            DE = (self.inventory_year - age)
-            row.setValue(self.inventory_field_names['establishment_date'], DE)
-            DistDate = row.getValue(self.inventory_field_names['disturbance_yr'])
-            yearDiff = (DE - DistDate)
-            inv_dist_dateDiff = self.inventory_field_names['dist_date_diff']
-            if DistDate > 0:
-                row.setValue(inv_dist_dateDiff, yearDiff)
-            else:
-                row.setValue(inv_dist_dateDiff, 0)
-            cur.updateRow(row)
- 
-
-class CalculateNewDistYr(object):
-    def __init__(self, arcpy, inventory_workspace, inventory_year,
-                 inventory_field_names, rollback_start,
-                 harv_yr_field, dist_age_prop_file_path):
-        self.arcpy = arcpy
-        self.inventory_workspace = inventory_workspace
-        self.inventory_field_names = inventory_field_names
-        self.rollback_start = rollback_start
-        self.inv_vintage = inventory_year
-        self.harv_yr_field = harv_yr_field
-        self.dist_age_prop_file_path = dist_age_prop_file_path
-        #Constants
-        self.DisturbedInventory = "DisturbedInventory"
-        self.disturbedInventory_layer = "disturbedInventory_layer"
-
-    def calculateNewDistYr(self):
-        logging.info("computing new disturbance years")
-        self.arcpy.env.workspace = self.inventory_workspace
-        self.arcpy.env.overwriteOutput = True
-        #local variables
-        self.inv_age_field = self.inventory_field_names['age']
-        self.establishment_date_field = self.inventory_field_names['establishment_date']
-        self.disturbance_yr = self.inventory_field_names['disturbance_yr']
-        self.new_disturbance_field = self.inventory_field_names['new_disturbance_yr']
-        self.inv_dist_date_diff_field = self.inventory_field_names['dist_date_diff']
-        self.dist_type_field = self.inventory_field_names['dist_type']
-        self.regen_delay_field = self.inventory_field_names['regen_delay']
-        self.preDistAge = self.inventory_field_names['pre_dist_age']
-        self.rollback_vintage_field = self.inventory_field_names['rollback_age']
-
-        self.makeLayers()
-        self.calculateDistType()
-        self.calculateRegenDelay()
-        self.calculatePreDistAge(self.dist_age_prop_file_path)
-        self.calculateRolledBackInvAge()
-
-    def makeLayers(self):
-        logging.info("making layers")
-        self.arcpy.MakeFeatureLayer_management(self.DisturbedInventory, self.disturbedInventory_layer)
-
-    def calculateDistType(self):
-        logging.info("calculating disturbance type")
-        cur = self.arcpy.UpdateCursor(self.disturbedInventory_layer)
-        for row in cur:
-            dist_year = row.getValue(self.disturbance_yr)
-            harv_year = row.getValue(self.harv_yr_field)
-            if dist_year == harv_year:
-                row.setValue(self.dist_type_field, "2")
-            else:
-                row.setValue(self.dist_type_field, "1")
-            cur.updateRow(row)
-
-    def calculateRegenDelay(self):
-        logging.info("calculating regen delay")
-
-        cur = self.arcpy.UpdateCursor(self.disturbedInventory_layer)
-        for row in cur:
-            age_diff = row.getValue(self.inv_dist_date_diff_field)
-            est_year = row.getValue(self.establishment_date_field)
-            dist_year = row.getValue(self.disturbance_yr)
-            inv_derived_disturbance = est_year #removed -1
-            if age_diff > 0:
-                row.setValue(self.regen_delay_field, age_diff)
-                row.setValue(self.new_disturbance_field, dist_year)
-            elif age_diff <= 0:
-                # Disturbance can't occur after establishment year - set to year before establishment.
-                row.setValue(self.regen_delay_field, 0)
-                row.setValue(self.new_disturbance_field, inv_derived_disturbance)
-
-            cur.updateRow(row)
-
-    def calculatePreDistAge(self, dist_age_prop_path):
-        logging.info("Calculating pre disturbance age using '{}' to select age"
-                     .format(dist_age_prop_path))
-        dist_age_props = {}
-        with open(dist_age_prop_path, "r") as age_prop_file:
-            reader = csv.reader(age_prop_file)
-            reader.next() # skip header
-            for dist_type, age, prop in reader:
-                dist_type = int(dist_type)
-                dist_ages = dist_age_props.get(dist_type)
-                if not dist_ages:
-                    dist_age_props[dist_type] = {}
-                    dist_ages = dist_age_props[dist_type]
-                dist_ages[age] = float(prop)
-
-        age_distributors = {}
-        for dist_type, age_props in dist_age_props.iteritems():
-            age_distributors[dist_type] = RollbackDistributor(**age_props)
-
-        cur = self.arcpy.UpdateCursor(self.disturbedInventory_layer)
-        for row in cur:
-            dist_type = int(row.getValue(self.dist_type_field))
-            age_distributor = age_distributors.get(dist_type)
-            if not age_distributor:
-                print "No age distributor for layer disturbance type {} - skipping.".format(dist_type)
-                continue
-
-            row.setValue(self.preDistAge, age_distributor.next())
-            cur.updateRow(row)
-
-        for dist_type, age_distributor in age_distributors.iteritems():
-            logging.info("Age picks for disturbance type {}:{}".format(dist_type,str(age_distributor)))
-
-    def calculateRolledBackInvAge(self):
-        logging.info("calculating rollback inventory age")
-        cur = self.arcpy.UpdateCursor(self.disturbedInventory_layer)
-        for row in cur:
-            pre_DistAge = row.getValue(self.preDistAge)
-            dist_yearNew = row.getValue(self.new_disturbance_field)
-            row.setValue(self.rollback_vintage_field, (pre_DistAge + self.rollback_start - dist_yearNew))
-
-            cur.updateRow(row)
-        # print self.arcpy.GetMessages()
+import pgdata
 
 
 class RollbackDistributor(object):
@@ -187,6 +24,77 @@ class RollbackDistributor(object):
         age = self._rand.choice(self._ages)
         self._choices[age] += 1
         return int(age)
+
+
+class CalculateNewDistYr(object):
+    def __init__(self, inventory_workspace, inventory_year,
+                 inventory_field_names, rollback_start,
+                 harv_yr_field, dist_age_prop_file_path):
+
+        self.inventory_workspace = inventory_workspace
+        self.inventory_field_names = inventory_field_names
+        self.rollback_start = rollback_start
+        self.inv_vintage = inventory_year
+        self.harv_yr_field = harv_yr_field
+        self.dist_age_prop_file_path = dist_age_prop_file_path
+        self.db = pgdata.connect()
+
+    def calculateNewDistYr(self):
+        logging.info("computing new disturbance years")
+        self.inv_age_field = self.inventory_field_names['age']
+        self.establishment_date_field = self.inventory_field_names['establishment_date']
+        self.disturbance_yr = self.inventory_field_names['disturbance_yr']
+        self.new_disturbance_field = self.inventory_field_names['new_disturbance_yr']
+        self.inv_dist_date_diff_field = self.inventory_field_names['dist_date_diff']
+        self.dist_type_field = self.inventory_field_names['dist_type']
+        self.regen_delay_field = self.inventory_field_names['regen_delay']
+        self.preDistAge = self.inventory_field_names['pre_dist_age']
+        self.rollback_vintage_field = self.inventory_field_names['rollback_age']
+
+        self.calculatePreDistAge(self.dist_age_prop_file_path)
+        self.calculateRolledBackInvAge()
+
+    def calculatePreDistAge(self, dist_age_prop_path):
+        """
+        This takes time to apply - one execution per grid cell record
+
+        It should be possible to apply the update to all cells at once.
+        Maybe using this approach?
+        https://dba.stackexchange.com/questions/55363/set-random-value-from-set/55364#55364
+        """
+        logging.info("Calculating pre disturbance age using '{}' to select age"
+                     .format(dist_age_prop_path))
+        dist_age_props = {}
+        with open(dist_age_prop_path, "r") as age_prop_file:
+            reader = csv.reader(age_prop_file)
+            reader.next()  # skip header
+            for dist_type, age, prop in reader:
+                dist_type = int(dist_type)
+                dist_ages = dist_age_props.get(dist_type)
+                if not dist_ages:
+                    dist_age_props[dist_type] = {}
+                    dist_ages = dist_age_props[dist_type]
+                dist_ages[age] = float(prop)
+        age_distributors = {}
+        for dist_type, age_props in dist_age_props.iteritems():
+            age_distributors[dist_type] = RollbackDistributor(**age_props)
+        for row in self.db['preprocessing.disturbed_inventory']:
+            age_distributor = age_distributors.get(dist_type)
+            #logging.info("Age picks for disturbance type {}:{}".format(dist_type, str(age_distributor)))
+            sql = """
+                UPDATE preprocessing.disturbed_inventory
+                SET pre_dist_age = %s
+                WHERE grid_id = %s
+            """
+            self.db.execute(sql, (age_distributor.next(), row['grid_id']))
+
+    def calculateRolledBackInvAge(self):
+        logging.info("calculating rollback inventory age")
+        sql = """
+            UPDATE preprocessing.disturbed_inventory
+            SET rollback_age = pre_dist_age + %s - new_disturbance_year
+        """
+        self.db.execute(sql)
 
 
 class updateInvRollback(object):
@@ -255,7 +163,7 @@ class updateInvRollback(object):
 
     def rollbackAgeNonDistStands(self):
         logging.info('Rolling back ages for age{}'.format(self.rollback_start))
-        
+
         cur = self.arcpy.UpdateCursor(self.RolledBackInventory_layer)
         for row in cur:
             rolledBackAge = row.getValue(self.rollback_vintage_field)
@@ -271,7 +179,7 @@ class updateInvRollback(object):
         year_range = range(self.rollback_range[0], self.rollback_range[1]+1)
         # print "Start of slashburn processing..."
         PercSBofCC = self.slashburn_percent
-        
+
         self.arcpy.MakeFeatureLayer_management(self.RolledBackInventory_layer, "temp_rollback")
         expression1 = '{} = {}'.format(self.arcpy.AddFieldDelimiters("temp_rollback", self.dist_type_field), 2)
         logging.info('Making slashburn for the range {}-{}'.format(self.rollback_range[0],self.rollback_range[1]))
