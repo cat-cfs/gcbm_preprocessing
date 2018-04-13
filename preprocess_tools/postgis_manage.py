3a47fcb7fd38cbbbb138fd6addda1ff6106d05fd
Scott Morken <smorken@nrcan.gc.ca>
2018-04-13 11:28:04 -0700

    allow create database parameters LC_CTYPE and LC_COLLATE fall through to default values since they appear to be system dependent

--- preprocess_tools/postgis_manage.py
+++ preprocess_tools/postgis_manage.py
@@ -82,8 +82,6 @@ def create_database(var_path, dbname):
                 WITH 
                 OWNER = postgres
                 ENCODING = 'UTF8'
-                LC_COLLATE = 'English_Canada.1252'
-                LC_CTYPE = 'English_Canada.1252'
                 TABLESPACE = pg_default
                 CONNECTION LIMIT = -1;
                 """, (AsIs(dbname),))
